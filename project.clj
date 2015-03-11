6b0b794e28ccfc3ee6d4f25b6da34853dff3be06
Aaron Santos <aaronds109@gmail.com>
2015-03-10 23:05:06 -0700

    Use cljx to create cljs source

--- project.clj
+++ project.clj
@@ -5,4 +5,30 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-tuple "0.1.2"]
-                 [org.clojure/data.priority-map "0.0.5"]])
+                 [org.clojure/data.priority-map "0.0.5"]]
+  :source-paths
+  ["src/clj_tiny_astar"
+   "target/generated-src/clj"
+   "target/generated-src/cljs"]
+
+
+  :cljx
+  {:builds [{:source-paths ["src/clj_tiny_astar"]
+             :output-path "target/generated-src/clj"
+             :rules :clj}
+            {:source-paths ["src/clj_tiny_astar"]
+             :output-path "target/generated-src/cljs"
+             :rules :cljs}]}
+
+  :prep-tasks [["cljx" "once"]]
+
+  :profiles {
+    :dev {:dependencies
+          [#_[org.clojure/clojurescript "0.0-3030"]]
+
+          :plugins
+          [[com.keminglabs/cljx "0.6.0"]
+           #_[com.cemerick/piggieback "0.1.5-SNAPSHOT"]
+           #_[lein-cljsbuild "1.0.4"]]}})
+
+
